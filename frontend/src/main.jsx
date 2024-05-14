2a760cfd939f3346072ea6ec069829f0843219d9
fazlulkarimhridoy <fkhridoy4321@gmail.com>
2024-05-15 02:30:03 +0600

    installed tanstack query and axios as well as designed home ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import {
 import Root from './Root';
 import ErrorPage from './components/ErrorPage';
 import Home from './components/Home';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const router = createBrowserRouter([
   {
@@ -23,8 +24,12 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 )
